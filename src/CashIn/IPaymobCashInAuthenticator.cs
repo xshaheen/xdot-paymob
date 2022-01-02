ddf7dd3dcf0ad057a4a38f4d57e38004cf4bbf7e
Mahmoud Shaheen <mxshaheen@gmail.com>
2022-01-02 23:56:23 +0200

    Fix IPaymobCashInAuthenticator documentation

--- src/CashIn/IPaymobCashInAuthenticator.cs
+++ src/CashIn/IPaymobCashInAuthenticator.cs
@@ -10,11 +10,11 @@ namespace X.Paymob.CashIn;
 public interface IPaymobCashInAuthenticator {
     /// <summary>Request a new authentication token.</summary>
     /// <returns>Authentication token response.</returns>
-    /// <exception cref="HttpRequestException"></exception>
+    /// <exception cref="PaymobRequestException"></exception>
     [Pure] Task<CashInAuthenticationTokenResponse> RequestAuthenticationTokenAsync();
 
     /// <summary>Get authentication token from cache if is valid or request a new one.</summary>
     /// <returns>Authentication token, which is valid for 1 hour from the creation time.</returns>
-    /// <exception cref="HttpRequestException"></exception>
+    /// <exception cref="PaymobRequestException"></exception>
     [Pure] ValueTask<string> GetAuthenticationTokenAsync();
 }
