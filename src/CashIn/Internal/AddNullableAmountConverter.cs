450af7f36cda4f9b26fe1ab44d82e40f9b6f78a8
Mahmoud Shaheen <mxshaheen@gmail.com>
2023-10-25 00:16:23 +0300

    Enhance AddNullableAmountConverter

--- src/CashIn/Internal/AddNullableAmountConverter.cs
+++ src/CashIn/Internal/AddNullableAmountConverter.cs
@@ -5,9 +5,11 @@ namespace X.Paymob.CashIn.Internal;
 
 internal sealed class AddNullableAmountConverter : JsonConverter<string> {
     public override string Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options) {
-        return reader.TokenType.Equals(JsonTokenType.Number) && reader.TryGetDecimal(out decimal result)
-            ? result.ToString()
-            : "N/A";
+        return reader.TokenType switch {
+            JsonTokenType.String => reader.GetString() ?? "N/A",
+            JsonTokenType.Number => reader.GetDecimal().ToString(CultureInfo.InvariantCulture),
+            _ => "N/A",
+        };
     }
 
     public override void Write(Utf8JsonWriter writer, string value, JsonSerializerOptions options) {
